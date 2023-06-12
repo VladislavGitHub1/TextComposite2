49c49b4f238e5c256cfff514b4f7dfa45f0a56fb
Vlad <x375298126643@yandex.by>
2023-06-12 18:16:01 +0300

    12.06.23

--- src/main/java/com/chernenkov/task/composite/Symbol.java
+++ src/main/java/com/chernenkov/task/composite/Symbol.java
@@ -3,10 +3,18 @@ package com.chernenkov.task.composite;
 public class Symbol implements TextComponent{
     private char letter;
     private TextType type;
-
+    char[] punctuationMarks = {'.', ',', ';', ':', '!', '?', '-', '(', ')', '[', ']', '{', '}'};
     public Symbol(char letter) {
         this.letter = letter;
-        if (Character.isPunctuation(letter)){
+        boolean isPunctuation = false;
+        for (char mark : punctuationMarks) {
+            if (letter == mark) {
+                isPunctuation = true;
+                break;
+            }
+        }
+
+        if (isPunctuation){
             type = TextType.PUNCTUATION;
         }
         else {
